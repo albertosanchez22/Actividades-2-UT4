7215624e1c084186783a5a8834b9e0e66996fd5d
Paco Sanchez <Paco Sanchez@192.168.2.3>
2019-01-21 19:05:36 +0100

    Añado una linea de código

---
+++ Ejercicio 2-UT4/src/ejercicio/pkg2/ut4/Ejercicio2UT4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg2.ut4;
+
+/**
+ *
+ * @author Paco Sanchez
+ */
+public class Ejercicio2UT4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Este es un pryecto de prueba :D
+    }
+    
+}
